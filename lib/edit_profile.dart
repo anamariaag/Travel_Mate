4a1af58249ecb82de7f33e9413ac82fdb5b452a0
HAZIX0 <ar727788@iteso.mx>
2024-03-22 19:31:34 -0600

    profile and my itineraries

---
+++ lib/edit_profile.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatefulWidget {
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  TextEditingController _fullNameController = TextEditingController();
+  TextEditingController _locationController = TextEditingController();
+  TextEditingController _dobController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Edit Profile',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              controller: _fullNameController,
+              decoration: InputDecoration(labelText: 'Full Name'),
+            ),
+            TextFormField(
+              controller: _locationController,
+              decoration: InputDecoration(labelText: 'Location'),
+            ),
+            TextFormField(
+              controller: _dobController,
+              decoration: InputDecoration(labelText: 'Date of Birth'),
+              onTap: () async {
+                DateTime? pickedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1900),
+                  lastDate: DateTime.now(),
+                );
+                if (pickedDate != null) {
+                  setState(() {
+                    _dobController.text = pickedDate.toString();
+                  });
+                }
+              },
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  'Save',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/my_itineraries.dart
+++ lib/my_itineraries.dart
@@ -1,21 +1,76 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyItineraries());
+import 'package:travel_mate/new_itinerary.dart';
 
 class MyItineraries extends StatelessWidget {
-  MyItineraries({super.key});
+  MyItineraries({Key? key});
+
+  // Example itinerary data
+  final List<String> itineraries = [
+    'Itinerary 1',
+    'Itinerary 2',
+    'Itinerary 3',
+    'Itinerary 4',
+    'Itinerary 5',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "My Itineraries",
+          style: TextStyle(color: Colors.white),
         ),
-        body: Center(
-          child: Text('Hello World'),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(
+        itemCount: itineraries.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(itineraries[index]),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              color: Colors.red,
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Confirm Delete'),
+                      content: Text(
+                          'Are you sure you want to delete this itinerary?'),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Delete'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => NewItinerary()));
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
         ),
+        backgroundColor: Colors.blue,
       ),
     );
   }
