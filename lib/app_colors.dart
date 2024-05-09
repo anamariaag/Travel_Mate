5d6425ba105b1e50302a5701b4789d6c72d7aab4
Ana Maria <70419948+anamariaag@users.noreply.github.com>
2024-05-08 18:32:13 -0600

    register change (#10)

---
+++ lib/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color.fromARGB(255, 6, 120, 190); // Sky blue
+  static const primaryColorVariant =
+      Color.fromARGB(255, 30, 144, 255); // Darker shade of sky blue
+  static const secondaryColor =
+      Color.fromARGB(255, 102, 153, 204); // Cadet blue
+  static const secondaryColorVariant =
+      Color.fromARGB(255, 4, 51, 130); // Another shade of blue
+  static const errorColor = Color(0xFFFF0000); // Red
+  static const successColor = Color.fromARGB(255, 36, 189, 36); // Green
+  static const backgroundColor = Color(0xFFFFFFFF); // White
+  static const surfaceColor = Color.fromARGB(255, 208, 237, 250); // White
+  static const onPrimaryColor = Color.fromARGB(255, 255, 255, 255); // White
+  static const onSecondaryColor = Color(0xFF000000); // Black
+  static const onSurfaceColor = Color(0xFF000000); // Black
+  static const onErrorColor = Color(0xFFFFFFFF); // White
+  static const onBackgroundColor = Color(0xFF000000); // Black
+}
